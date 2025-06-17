af5c8e94779de530326fcaf3211aa266b0b4f342
devnova777 <devnova0821@gmail.com>
2025-06-17 10:57:43 -0500

    feat: add methods for network conversion and CLI arguments in Network enum

--- pccontext/enums/network_enum.py
+++ pccontext/enums/network_enum.py
@@ -1,10 +1,10 @@
 """
 Cardano Network Enum
 """
-
 from enum import Enum
+from typing import List, Optional
 
-__all__ = ["Network"]
+from pycardano import Network as PyCardanoNetwork
 
 
 class Network(Enum):
@@ -18,3 +18,29 @@ class Network(Enum):
     SANCHONET = "sanchonet"
     GUILDNET = "guildnet"
     CUSTOM = "custom"
+
+    def get_network(self) -> PyCardanoNetwork:
+        """
+        Returns the corresponding PyCardano Network enum.
+        """
+        if self == Network.MAINNET:
+            return PyCardanoNetwork.MAINNET
+        else:
+            return PyCardanoNetwork.TESTNET
+
+    def get_cli_network_args(self, network_magic_number: Optional[int] = None) -> List[str]:
+        """
+        Returns the CLI network argument for the Cardano network.
+        """
+        if self == Network.MAINNET:
+            return ["--mainnet"]
+        elif self == Network.PREPROD:
+            return ["--testnet-magic", str(1)]
+        elif self == Network.PREVIEW:
+            return ["--testnet-magic", str(2)]
+        elif self == Network.SANCHONET:
+            return ["--testnet-magic", str(4)]
+        elif self == Network.GUILDNET:
+            return ["--testnet-magic", str(141)]
+        else:
+            return ["--testnet-magic", str(network_magic_number)]
