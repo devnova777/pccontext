e36b253990249b56590bf94b396e102d113b79d7
devnova777 <devnova0821@gmail.com>
2025-07-30 08:10:10 -0500

    fix: pass initial transaction witness set to be updated rather than overwritten

--- pccontext/transactions/assemble.py
+++ pccontext/transactions/assemble.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Optional, Union
+from typing import Any, List, Optional, TypeVar, Union
 
 from pycardano import (
     NativeScript,
@@ -8,9 +8,32 @@ from pycardano import (
     PlutusV3Script,
     Redeemers,
     Transaction,
-    TransactionWitnessSet,
     VerificationKeyWitness,
 )
+
+T = TypeVar("T")
+
+
+def _update_witness_set(
+    witness_set_attr: Optional[Union[List[T], NonEmptyOrderedSet[T]]],
+    new_witnesses: Union[List[T], NonEmptyOrderedSet[T]],
+) -> Union[List[T], NonEmptyOrderedSet[T]]:
+    """Helper function to update a witness set with new witnesses."""
+    if witness_set_attr is None:
+        return new_witnesses
+
+    if isinstance(new_witnesses, list):
+        if isinstance(witness_set_attr, list):
+            witness_set_attr.extend(new_witnesses)
+        else:
+            witness_set_attr.extend(list(new_witnesses))
+    elif isinstance(new_witnesses, NonEmptyOrderedSet):
+        if isinstance(witness_set_attr, list):
+            witness_set_attr.extend(list(new_witnesses))
+        else:
+            witness_set_attr.extend(list(new_witnesses))
+
+    return witness_set_attr
 
 
 def assemble_transaction(
@@ -48,15 +71,38 @@ def assemble_transaction(
     :param plutus_v3_script: Optional list of Plutus V3 scripts.
     :return: The assembled transaction.
     """
-    transaction.transaction_witness_set = TransactionWitnessSet(
-        vkey_witnesses=vkey_witnesses,
-        native_scripts=native_scripts,
-        bootstrap_witness=bootstrap_witness,
-        plutus_v1_script=plutus_v1_script,
-        plutus_data=plutus_data,
-        redeemer=redeemer,
-        plutus_v2_script=plutus_v2_script,
-        plutus_v3_script=plutus_v3_script,
-    )
+    if vkey_witnesses:
+        transaction.transaction_witness_set.vkey_witnesses = _update_witness_set(
+            transaction.transaction_witness_set.vkey_witnesses, vkey_witnesses
+        )
+
+    if native_scripts:
+        transaction.transaction_witness_set.native_scripts = _update_witness_set(
+            transaction.transaction_witness_set.native_scripts, native_scripts
+        )
+
+    if bootstrap_witness is not None:
+        transaction.transaction_witness_set.bootstrap_witness.extend(bootstrap_witness)
+
+    if plutus_data is not None:
+        transaction.transaction_witness_set.plutus_data.extend(plutus_data)
+
+    if redeemer is not None:
+        transaction.transaction_witness_set.redeemer = redeemer
+
+    if plutus_v1_script:
+        transaction.transaction_witness_set.plutus_v1_script = _update_witness_set(
+            transaction.transaction_witness_set.plutus_v1_script, plutus_v1_script
+        )
+
+    if plutus_v2_script:
+        transaction.transaction_witness_set.plutus_v2_script = _update_witness_set(
+            transaction.transaction_witness_set.plutus_v2_script, plutus_v2_script
+        )
+
+    if plutus_v3_script:
+        transaction.transaction_witness_set.plutus_v3_script = _update_witness_set(
+            transaction.transaction_witness_set.plutus_v3_script, plutus_v3_script
+        )
 
     return transaction
--- tests/transactions/test_assemble.py
+++ tests/transactions/test_assemble.py
@@ -1,14 +1,48 @@
+from dataclasses import dataclass
+
 from pycardano import (
     Address,
+    AuxiliaryData,
+    ExecutionUnits,
+    IndefiniteList,
+    InvalidBefore,
+    InvalidHereAfter,
+    Metadata,
+    NativeScript,
+    NonEmptyOrderedSet,
     PaymentSigningKey,
     PaymentVerificationKey,
+    PlutusData,
+    PlutusV1Script,
+    PlutusV2Script,
+    PlutusV3Script,
+    Redeemer,
+    RedeemerTag,
+    ScriptAll,
+    ScriptPubkey,
     StakeSigningKey,
     StakeVerificationKey,
+    VerificationKey,
     VerificationKeyWitness,
 )
 
 from pccontext.transactions.assemble import assemble_transaction
 from pccontext.transactions.stake_address_registration import stake_address_registration
+
+
+@dataclass
+class MyTest(PlutusData):
+    CONSTR_ID = 130
+
+    a: int
+    b: bytes
+    c: IndefiniteList
+    d: dict
+
+
+@dataclass
+class MyRedeemer(Redeemer):
+    data: MyTest
 
 
 def test_assemble_transaction(
@@ -22,11 +56,64 @@ def test_assemble_transaction(
     stake_skey = StakeSigningKey.generate()
     stake_vkey = StakeVerificationKey.from_signing_key(stake_skey)
 
+    other_payment_skey = PaymentSigningKey.generate()
+    other_payment_vkey = PaymentVerificationKey.from_signing_key(other_payment_skey)
+
     address = Address(
         payment_vkey.hash(),
         stake_vkey.hash(),
     )
     transaction = stake_address_registration(chain_context, stake_vkey, address)
+
+    auxiliary_data = AuxiliaryData(
+        data=Metadata(
+            {
+                674: {"MySwap": "Market Order"},
+            }
+        )
+    )
+    transaction.auxiliary_data = auxiliary_data
+
+    vk1 = VerificationKey.from_cbor(
+        "58206443a101bdb948366fc87369336224595d36d8b0eee5602cba8b81a024e58473"
+    )
+    vk2 = VerificationKey.from_cbor(
+        "58206443a101bdb948366fc87369336224595d36d8b0eee5602cba8b81a024e58475"
+    )
+    spk1 = ScriptPubkey(key_hash=vk1.hash())
+    spk2 = ScriptPubkey(key_hash=vk2.hash())
+    before = InvalidHereAfter(123456789)
+    after = InvalidBefore(123456780)
+    script = ScriptAll([before, after, spk1, spk2])
+    native_scripts = [script]
+    transaction.transaction_witness_set.native_scripts = native_scripts
+
+    data = MyTest(123, b"1234", IndefiniteList([4, 5, 6]), {1: b"1", 2: b"2"})
+
+    plutus_data = [data]
+    transaction.transaction_witness_set.plutus_data = plutus_data
+
+    redeemer = MyRedeemer(data, ExecutionUnits(1000000, 1000000))
+    redeemer.tag = RedeemerTag.SPEND
+    redeemers = [redeemer]
+    transaction.transaction_witness_set.redeemer = redeemers
+
+    plutus_v1_script = NonEmptyOrderedSet([PlutusV1Script(b"magic script v1")])
+    plutus_v2_script = NonEmptyOrderedSet([PlutusV2Script(b"magic script v2")])
+    plutus_v3_script = NonEmptyOrderedSet([PlutusV3Script(b"magic script v3")])
+    transaction.transaction_witness_set.plutus_v1_script = plutus_v1_script
+    transaction.transaction_witness_set.plutus_v2_script = plutus_v2_script
+    transaction.transaction_witness_set.plutus_v3_script = plutus_v3_script
+
+    initial_vkey_witnesses = NonEmptyOrderedSet(
+        [
+            VerificationKeyWitness(
+                other_payment_vkey,
+                other_payment_skey.sign(transaction.transaction_body.hash()),
+            )
+        ]
+    )
+    transaction.transaction_witness_set.vkey_witnesses = initial_vkey_witnesses
 
     vkey_witnesses = [
         VerificationKeyWitness(
@@ -45,4 +132,11 @@ def test_assemble_transaction(
     assert assembled_tx is not None
     assert assembled_tx.transaction_body == transaction.transaction_body
     assert assembled_tx.valid is True
-    assert len(assembled_tx.transaction_witness_set.vkey_witnesses) == 2
+    assert assembled_tx.auxiliary_data == auxiliary_data
+    assert assembled_tx.transaction_witness_set.native_scripts == native_scripts
+    assert assembled_tx.transaction_witness_set.plutus_data == plutus_data
+    assert assembled_tx.transaction_witness_set.redeemer == redeemers
+    assert assembled_tx.transaction_witness_set.plutus_v1_script == plutus_v1_script
+    assert assembled_tx.transaction_witness_set.plutus_v2_script == plutus_v2_script
+    assert assembled_tx.transaction_witness_set.plutus_v3_script == plutus_v3_script
+    assert len(assembled_tx.transaction_witness_set.vkey_witnesses) == 3
